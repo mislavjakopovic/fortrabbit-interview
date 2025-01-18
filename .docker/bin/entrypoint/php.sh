69f67993e49b07ad21b41decfc8de5fcec27a877
Mislav Jakopović <mislavjakopovic6@gmail.com>
2025-01-18 23:25:36 +0100

    chore: update php entrypoint script

--- .docker/bin/entrypoint/php.sh
+++ .docker/bin/entrypoint/php.sh
@@ -9,23 +9,29 @@ start_init "php"
 info_section "Waiting for services"
 wait "mariadb"
 
-#info_section "Installing dependencies.. $(pwd)"
-#run "composer install"
+info_section "Installing dependencies.. $(pwd)"
+run "composer install"
 
-#info_section "Warming up cache.."
-#run "bin/console cache:warmup"
+info_section "Warming up cache.."
+run "bin/console cache:warmup"
 
-#info "Initializing Doctrine.."
-#run "bin/console doctrine:migrations:migrate --no-interaction"
+info "Applying Doctrine migrations for dev environment.."
+run "bin/console doctrine:migrations:migrate --no-interaction"
 
-#info "Generating JWT keys.."
-#run "bin/console lexik:jwt:generate-keypair --skip-if-exists"
+info "Applying fixtures.."
+run "bin/console doctrine:fixtures:load --group=dev --no-interaction"
 
-#info "Applying fixtures.."
-#run "bin/console doctrine:fixtures:load --group=dev --append --no-interaction"
+info "Creating database for test environment.."
+run "bin/console doctrine:database:create --env=test --if-not-exists"
+
+info "Applying Doctrine migrations for test environment.."
+run "bin/console doctrine:migrations:migrate --env=test --no-interaction"
 
 info "Setting user rights.."
 run "chown -R www-data:www-data $SETUP_APP_DIR"
+
+info "Checking installed libraries for security vulnerabilities.."
+run "composer audit"
 
 end_init "php"
 exec "$@"
